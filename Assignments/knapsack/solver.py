1a58f64e5473c997af5a06a95a5211b4372de14f
PratikPedhavi <pratikpdv@gmail.com>
2022-03-05 14:01:35 +0530

    Using Dynamic Programming

--- Assignments/knapsack/solver.py
+++ Assignments/knapsack/solver.py
@@ -27,8 +27,13 @@ def solve_it(input_data):
     weight = 0
     taken = [0]*len(items)
 
-    value, outlist = optimal_comb(items,capacity,item_count-1,[])
-    taken = [1 if k in outlist else taken[k] for k in range(len(taken))]
+    # Using Recursion
+    # value, outlist = optimal_comb(items,capacity,item_count-1,[])
+    # taken = [1 if k in outlist else taken[k] for k in range(len(taken))]
+
+    # Using Dynamic Programming
+    value, taken = dynamic_prog(items, capacity)
+
     # prepare the solution in the specified output format
     output_data = str(value) + ' ' + str(0) + '\n'
     output_data += ' '.join(map(str, taken))
@@ -50,12 +55,34 @@ def optimal_comb(items, cap, item, outlist):
         skipped = optimal_comb(items,cap,item-1,outlist)
         return skipped[0], outlist + skipped[1]
 
+def dynamic_prog(items, cap):
+    value_dict = {(i,j):0 for i in range(len(items)+1) for j in range(int(cap)+1)}
+    for j in range(int(cap)+1):
+        for item in items:
+            # if item.index > 0:
+            if item.weight <= j:
+                value_dict[(item.index+1,j)] = max(value_dict[(item.index, j)], item.value + value_dict[(item.index, j - item.weight)])
+            else:
+                value_dict[(item.index+1,j)] = value_dict[(item.index,j)]
+    # BACKTRACKER
+    selection = []
+    temp_cap = cap
+    for item in items[::-1]:
+        # if item.index > 0:
+        if value_dict[(item.index+1,temp_cap)] == value_dict[(item.index,temp_cap)]:
+            selection.append(0)
+        else:
+            selection.append(1)
+            temp_cap -= item.weight
+    return value_dict[(len(items),cap)],selection
+
+
 if __name__ == '__main__':
     import sys
-    if len(sys.argv) > 1:
-        file_location = sys.argv[1].strip()
-    # if 1:
-    #     file_location = r'C:\Users\Pradnya\Documents\Work\Coursera\DiscreteOptimization\Assignments\knapsack\data\ks_4_0'
+    # if len(sys.argv) > 1:
+    #     file_location = sys.argv[1].strip()
+    if 1:
+        file_location = r'C:\Users\Pradnya\Documents\Work\Coursera\DiscreteOptimization\Assignments\knapsack\data\ks_50_0'
         with open(file_location, 'r') as input_data_file:
             input_data = input_data_file.read()
         print(solve_it(input_data))
