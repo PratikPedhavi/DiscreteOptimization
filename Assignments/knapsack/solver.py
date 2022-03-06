92db8323968c6c6e8279b6515cd941f49e7789dd
PratikPedhavi <pratikpdv@gmail.com>
2022-03-06 17:13:24 +0530

    depth first exhaustive

--- Assignments/knapsack/solver.py
+++ Assignments/knapsack/solver.py
@@ -21,6 +21,8 @@ def solve_it(input_data):
         parts = line.split()
         items.append(Item(i-1, int(parts[0]), int(parts[1])))
 
+    print('Weights: ', [item.weight for item in items])
+    print('Values: ', [item.value for item in items])
     # a trivial algorithm for filling the knapsack
     # it takes items in-order until the knapsack is full
     value = 0
@@ -32,7 +34,10 @@ def solve_it(input_data):
     # taken = [1 if k in outlist else taken[k] for k in range(len(taken))]
 
     # Using Dynamic Programming
-    value, taken = dynamic_prog(items, capacity)
+    # value, taken = dynamic_prog(items, capacity)
+
+    # Depth First Branch and Bound
+    value,selection = depth_first(items, capacity)
 
     # prepare the solution in the specified output format
     output_data = str(value) + ' ' + str(0) + '\n'
@@ -76,13 +81,56 @@ def dynamic_prog(items, cap):
             temp_cap -= item.weight
     return value_dict[(len(items),cap)],selection
 
+def depth_first(items, cap):
+    selection = [0 for k in range(len(items))]
+    value = 0
+    room = cap
+    estimate = sum([item.value for item in items])
+    cls_bnb = branchNbound(selection,items)
+    _ = cls_bnb.traverse(items[0],value,room,estimate)
+    value,selection = cls_bnb.max_value, cls_bnb.selection
+    return value, selection
+
+class branchNbound():
+    def __init__(self,selection,items):
+        self.items = items
+        self.selection = selection
+        self.max_value = 0
+    def traverse(self,item,value,room,estimate,slct= []):
+        if estimate >= value:
+            for branch in [1,0]:
+                child_value = value
+                child_room = room
+                child_slct = slct.copy()
+                if item.weight <= room and branch:
+                    brnch = 'left'
+                    child_value = value + item.value
+                    child_room = room - item.weight
+                    child_slct.append(item.index)
+                elif not branch:
+                    child_value = value
+                    child_room = room
+                    brnch = 'right'
+                    estimate -= item.value
+                else:
+                    continue
+                if self.max_value <= child_value:
+                    self.max_value = child_value
+                    self.selection = child_slct
+                print("Item: ",item.index, 'Max Val: ', self.max_value, 'Max Select: ', self.selection,
+                    'Value: ',child_value, 'Room: ',child_room, 'Estimate: ',estimate, 'Branch: ', 
+                    brnch, 'Selected: ',child_slct)
+                if item.index < len(self.items) - 1:
+                    child_value,child_slct = self.traverse(self.items[item.index+1],child_value,child_room,estimate,child_slct)
+            value = max(child_value,value)
+        return value, child_slct
 
 if __name__ == '__main__':
     import sys
     # if len(sys.argv) > 1:
     #     file_location = sys.argv[1].strip()
     if 1:
-        file_location = r'C:\Users\Pradnya\Documents\Work\Coursera\DiscreteOptimization\Assignments\knapsack\data\ks_50_0'
+        file_location = r'C:\Users\Pradnya\Documents\Work\Coursera\DiscreteOptimization\Assignments\knapsack\data\ks_4_0_custom'
         with open(file_location, 'r') as input_data_file:
             input_data = input_data_file.read()
         print(solve_it(input_data))
